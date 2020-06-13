e07e6b993ab52778dc92d344c6b22a5eec166c28
Helmy Fikri Husein <helmyfikrih@gmail.com>
2020-06-13 23:57:04 +0700

    add filtering place by user

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import RoomIcon from "@material-ui/icons/Room";
+import Box from "@material-ui/core/Box";
 
 import PlaceItem from './PlaceItem';
 import './PlaceList.css';
@@ -17,6 +22,9 @@ const useStyles = makeStyles((theme) => ({
   control: {
     padding: theme.spacing(2),
   },
+  button: {
+    margin: theme.spacing(1),
+  },
 }));
 
 const PlaceList = props => {
@@ -24,10 +32,32 @@ const PlaceList = props => {
 
     if(props.items.length===0){
       return (
-        <div>
-          <h2>No Place found. Maybe create one ?</h2>
-          <button>Share Place</button>
-        </div>
+        <Grid container className={classes.root} spacing={2}>
+          <Grid item xs={12}>
+            <Grid container justify='center' spacing={2}>
+              <Card>
+                <CardContent>
+                  <Box component='span' m={1}>
+                    <h2>
+                      <div className={classes.root}>
+                        {"No Place found. Maybe create one ?"}
+                      </div>
+                    </h2>
+                  </Box>
+                  <div>
+                    <Button
+                      variant='contained'
+                      color='primary'
+                      className={classes.button}
+                      startIcon={<RoomIcon />}>
+                      Share Place
+                    </Button>
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
       );
     }
 
